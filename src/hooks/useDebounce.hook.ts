0836dc1d38ef470b8c26ff2b42176291819b9618
NhutMnhIT <nhutminh.it19@gmail.com>
2024-12-26 10:17:48 +0700

    refactor: change use debounce

--- src/hooks/useDebounce.hook.ts
+++ src/hooks/useDebounce.hook.ts
@@ -1,19 +1,5 @@
 import { useCallback, useRef, useEffect } from 'react';
 
-function debounce<T extends (...args: any[]) => void>(
-    func: T,
-    wait: number
-): (...args: Parameters<T>) => void {
-    let timeoutId: NodeJS.Timeout | undefined;
-
-    return function (this: any, ...args: Parameters<T>): void {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            func.apply(this, args);
-        }, wait);
-    };
-}
-
 export const useDebounce = <T extends (...args: any[]) => void>(
     func: T,
     wait: number,
