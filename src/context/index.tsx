1509fada405f5bd072d330a169dc02e524387f6f
NhutMnhIT <nhutminh.it19@gmail.com>
2025-01-03 15:11:57 +0700

    refactor: remove user context, using api service

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { AuthProvider } from "./auth/auth.context";
-import { UserProvider } from "../pages/user-management-page/context/user-management.context";
 
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <AuthProvider>
-            <UserProvider>
-                {children}
-            </UserProvider>
+            {children}
         </AuthProvider>
     );
 };
