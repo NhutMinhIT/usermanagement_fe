ae7e008dced01d724d28d1c6aeff7ab8f1b7969a
MinhNN-Aris <minh.nn@aris-vn.com>
2025-01-06 14:36:26 +0700

    refactor: enhance user form validation with constants for error messages

--- src/pages/user-management-page/schemas/create-user-schema.ts
+++ src/pages/user-management-page/schemas/create-user-schema.ts
@@ -1,3 +1,4 @@
+import { EMAIL_REGEX, ERROR_EMAIL_INVALID_FORMAT, ERROR_EMAIL_REQUIRED, ERROR_FULLNAME_MAX_LENGTH, ERROR_FULLNAME_MIN_LENGTH, ERROR_FULLNAME_REQUIRED, ERROR_INVALID_ROLE, ERROR_ROLE_REQUIRED, ERROR_USERNAME_MAX_LENGTH, ERROR_USERNAME_MIN_LENGTH, ERROR_USERNAME_REQUIRED, VALID_ROLES } from './../constant/index';
 import { ICreateUser } from "../types/user-managment.type";
 
 
@@ -9,13 +10,37 @@ interface ValidationErrors {
 export const validateUserForm = (data: ICreateUser): ValidationErrors => {
     const errors: ValidationErrors = {};
 
-    // Validation rules
-    if (!data.username?.trim()) {
-        errors.username = 'Username is required';
+    // Username validation
+    if (!data.username) {
+        errors.username = ERROR_USERNAME_REQUIRED;
+    } else if (data.username.length < 3) {
+        errors.username = ERROR_USERNAME_MIN_LENGTH;
+    } else if (data.username.length > 50) {
+        errors.username = ERROR_USERNAME_MAX_LENGTH
     }
 
-    if (!data.email?.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-        errors.email = 'Invalid email format';
+    // Full name validation
+    if (!data.fullName) {
+        errors.fullName = ERROR_FULLNAME_REQUIRED;
+    } else if (data.fullName.length < 2) {
+        errors.fullName = ERROR_FULLNAME_MIN_LENGTH;
+    } else if (data.fullName.length > 50) {
+        errors.fullName = ERROR_FULLNAME_MAX_LENGTH;
+    }
+
+    // Email validation
+    const emailRegex = EMAIL_REGEX
+    if (!data.email) {
+        errors.email = ERROR_EMAIL_REQUIRED
+    } else if (!emailRegex.test(data.email)) {
+        errors.email = ERROR_EMAIL_INVALID_FORMAT
+    }
+    // Role validation
+    const validRoles = VALID_ROLES
+    if (!data.role) {
+        errors.role = ERROR_ROLE_REQUIRED
+    } else if (!validRoles.includes(data.role)) {
+        errors.role = ERROR_INVALID_ROLE
     }
 
     // Return collected errors
